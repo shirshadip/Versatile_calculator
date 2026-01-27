c282892d5af6bed47b16d2c0933a8ad08e98ea28
shirshadip <artistavra@gmail.com>
2026-01-27 23:35:10 +0530

    permutations and combinations added

--- javascript/p&c.js
+++ javascript/p&c.js
@@ -20,3 +20,29 @@ function showfact(){
     const result=fact(number);
     document.getElementById('display').innerText=`${number}!= ${result}`
 }
+function calcpermutations(x,y){
+    let down = x-y;
+    let up= x;
+    let result= fact(up)/fact(down);
+    return result;
+}
+function showpermutations(){
+    const nforperm=Number(document.getElementById("nforp").value);
+    const rforperm=Number(document.getElementById("rforp").value);
+    const result= calcpermutations(nforperm,rforperm);
+    document.getElementById('displayp').innerHTML=`<p><sup>${nforperm}</sup>P<sub>${rforperm}</sub> = ${result}</p>`;
+
+}
+function calccombinations(x,y){
+    let down = x-y;
+    let up= x;
+    let result= fact(up)/(fact(y)*fact(down));
+    return result;
+}
+function showcombinations(){
+    const nforcomb=Number(document.getElementById("nforc").value);
+    const rforcomb=Number(document.getElementById("rforc").value);
+    const result= calccombinations(nforcomb,rforcomb);
+    document.getElementById('displayc').innerHTML=`<p><sup>${nforcomb}</sup>c<sub>${rforcomb}</sub> = ${result}</p>`;
+
+}
