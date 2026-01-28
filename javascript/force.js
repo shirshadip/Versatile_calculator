876431d524c6463ee644e89356f5da27dd8d0f32
shirshadip <artistavra@gmail.com>
2026-01-28 23:36:31 +0530

    unit added

--- javascript/force.js
+++ javascript/force.js
@@ -1,9 +1,18 @@
-function showfma() {
+function showfmaSI() {
     const m = Number(document.getElementById('m1').value);
     const a = Number(document.getElementById('a1').value);
 
     const result = m*a;
-    document.getElementById('displayfma').innerHTML=`
-    <p>F=${m}x${a} is =>${result}</p>
+    document.getElementById('displayfmaSI').innerHTML=`
+    <p>F=${m}x${a} is =>${result} Newton</p>
+    `;
+}
+function showfmaCGS() {
+    const m = Number(document.getElementById('m1').value);
+    const a = Number(document.getElementById('a1').value);
+
+    const result = m*a;
+    document.getElementById('displayfmaCGS').innerHTML=`
+    <p>F=${m}x${a} is =>${result} Dyne</p> 
     `;
 }
